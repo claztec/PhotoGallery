c550755f44e803b0014000fd3c8383c72c64becc
Jaeman Choi <claztec@gmail.com>
2016-09-18 21:28:14 +0900

    Get server data

--- app/src/main/java/net/claztec/android/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/net/claztec/android/photogallery/PhotoGalleryFragment.java
@@ -1,13 +1,61 @@
 package net.claztec.android.photogallery;
 
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.io.IOException;
 
 /**
  * Created by Derek Choi on 2016. 9. 18..
  */
 public class PhotoGalleryFragment extends Fragment {
 
+    public static final String TAG = "PhotoGalleryFragment";
+
+    private RecyclerView mPhotoRecyclerView;
+
     public static PhotoGalleryFragment getInstance() {
         return new PhotoGalleryFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+
+        mPhotoRecyclerView = (RecyclerView) v.findViewById(R.id.fragment_photo_gallery_recycler_view);
+        mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+
+        return v;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+        new FetchItemsTask().execute();
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                String result = new FlickrFetchr().getUrlString("http://blog.claztec.net");
+                Log.i(TAG, "Fetched contents of URL: " + result);
+            } catch (IOException ioe) {
+                Log.e(TAG, "Failed to fetch URL: " + ioe);
+            }
+
+            return null;
+        }
     }
 }
