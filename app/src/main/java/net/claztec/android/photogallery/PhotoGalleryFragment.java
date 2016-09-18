dbc48f44b62ea929c9a0f3b6af6a04fc63328921
Jaeman Choi <claztec@gmail.com>
2016-09-18 10:57:33 +0900

    Set fragment

---
+++ app/src/main/java/net/claztec/android/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,13 @@
+package net.claztec.android.photogallery;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Derek Choi on 2016. 9. 18..
+ */
+public class PhotoGalleryFragment extends Fragment {
+
+    public static PhotoGalleryFragment getInstance() {
+        return new PhotoGalleryFragment();
+    }
+}
