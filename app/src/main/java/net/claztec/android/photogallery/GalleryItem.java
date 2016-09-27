8e5f5c95d4cbb7af09ecee6a88c859cc6a9b898c
Derek <claztec@gmail.com>
2016-09-27 21:05:05 +0900

    Set photo detail link

--- app/src/main/java/net/claztec/android/photogallery/GalleryItem.java
+++ app/src/main/java/net/claztec/android/photogallery/GalleryItem.java
@@ -1,5 +1,7 @@
 package net.claztec.android.photogallery;
 
+import android.net.Uri;
+
 /**
  * Created by Derek Choi on 2016. 9. 18..
  */
@@ -7,6 +9,7 @@ public class GalleryItem {
     private String mCaption;
     private String mId;
     private String mUrl;
+    private String mOwner;
 
     public String getCaption() {
         return mCaption;
@@ -30,6 +33,22 @@ public class GalleryItem {
 
     public void setUrl(String url) {
         mUrl = url;
+    }
+
+    public String getOwner() {
+        return mOwner;
+    }
+
+    public void setOwner(String owner) {
+        mOwner = owner;
+    }
+
+    public Uri getPhotoPageUri() {
+        return Uri.parse("http://flickr.com/photos/")
+                .buildUpon()
+                .appendPath(mOwner)
+                .appendPath(mId)
+                .build();
     }
 
     @Override
